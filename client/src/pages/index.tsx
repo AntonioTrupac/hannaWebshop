9e89debc3302ce53ba71af66ada5f72fb9c4914d
AntonioTrupac <sniper.trupac@gmail.com>
2022-01-11 23:09:13 +0100

    added prettier

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,4 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from "@chakra-ui/react";
-import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+import { Text } from "@chakra-ui/react";
 
 import { Hero } from "../components/Hero";
 import { Box } from "@chakra-ui/react";
@@ -14,7 +6,6 @@ import { Box } from "@chakra-ui/react";
 import { Container } from "../components/Container";
 import { Main } from "../components/Main";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
-import { CTA } from "../components/CTA";
 import { Footer } from "../components/Footer";
 
 const Index = () => (
