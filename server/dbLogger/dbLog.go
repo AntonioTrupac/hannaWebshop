07fc0216f201d628302b2f1abd6cdfa768504d91
AntonioTrupac <sniper.trupac@gmail.com>
2022-01-15 14:41:01 +0100

    Connected GORM with PlanetScale, structured folders a bit, need to create models now

---
+++ server/dbLogger/dbLog.go
@@ -0,0 +1,21 @@
+package logDb
+
+import (
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
+)
+
+func DbLog() logger.Interface {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+	return newLogger
+}
--- server/main.go
+++ server/main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
+	db "github.com/AntonioTrupac/hannaWebshop/database"
 )
 
 func main() {
-	fmt.Println("Connecting")
 
-	db, err := sql.Open("mysql", "ql6e013y0w78:pscale_pw_m0h2YiWrWT9HNiCx9y6816aqe-Yl0J64k_QEtODqF38@tcp(kqmwg2ezxey8.eu-west-3.psdb.cloud)/hannawebshop?tls=true")
+	db.Execute()
 
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// defer the close till after the main function has finished
-	// executing
-	defer db.Close()
-
-	insert, err := db.Query("INSERT INTO user VALUES ( 3, 'Karlo' )")
-
-	if err != nil {
-		panic(err.Error())
-	}
-	// be careful deferring Queries if you are using transactions
-	defer insert.Close()
 }
